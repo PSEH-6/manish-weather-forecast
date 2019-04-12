f4256617428b95076b6808b3cc2d43f30f0eb6aa
PSEH-6 <39406028+PSEH-6@users.noreply.github.com>
2019-04-12 15:10:07 +0530

    Add files via upload

--- src/test/java/com/sapient/weather/ManishWeatherForecastApplicationTests.java
+++ src/test/java/com/sapient/weather/ManishWeatherForecastApplicationTests.java
@@ -1,16 +1,29 @@
 package com.sapient.weather;
 
+import org.json.JSONException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ManishWeatherForecastApplicationTests {
 
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws JSONException {
+		String actual = restTemplate.getForObject("http://localhost:"+port+"/api/v1/forecast/Kanpur", String.class);
+		JSONAssert.assertEquals("{\"message\":\"Use sunscreen lotion\"}", actual, false);
 	}
 
 }
