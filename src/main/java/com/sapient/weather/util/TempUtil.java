f4787192d4c59d54da6e614493ab2b2f489a7139
PSEH-6 <39406028+PSEH-6@users.noreply.github.com>
2019-04-12 15:32:15 +0530

    Add files via upload

--- src/main/java/com/sapient/weather/util/TempUtil.java
+++ src/main/java/com/sapient/weather/util/TempUtil.java
@@ -3,7 +3,8 @@ package com.sapient.weather.util;
 public class TempUtil {
 	
 	public static double convertToCelsius(double faren) {
-		return (faren-32)*(5/9);
+		double cel = (faren-32)*(0.555d);
+		return cel;
 	}
 
 }
